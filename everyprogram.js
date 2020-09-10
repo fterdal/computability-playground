2452bb51d04dbe7a6579453df04f7c66c890ee0e
Finn <finnterdal@gmail.com>
2020-09-10 14:13:00 -0500

    demo logging every character from Unicode

--- everyprogram.js
+++ everyprogram.js
@@ -1,14 +1,62 @@
 // const child_process = require('child_process')
 // const eval =
 
-const valid = 'let a;'
+const valid = 'let A;';
 
-const invalid = 'let 1;'
+const invalid = 'let 1;';
 
-const logIt = "console.log('hello')"
+const logIt = "console.log('hello')";
 
 // child_process.execSync(logIt);
 // child_process.execSync(valid);
-// eval(valid);
-// eval(invalid);
-eval(logIt); // It logs!
+
+// try {
+//   eval(valid);
+//   eval(invalid);
+// } catch (err) {
+//   console.log(err);
+// }
+// eval(logIt); // It logs!
+
+for (let i = 32; i < 128; i++) {
+  console.log('i = ', i, ' => ', String.fromCharCode(i))
+}
+
+// const idx = 4
+
+// console.log(valid.charCodeAt(idx), valid[idx]);
+
+// const i = '-'
+// console.log(i.charCodeAt(0), i[0]);
+// console.log('i = ', i, ' => ', String.fromCharCode(i))
+
+// let alphabet = [
+//   [
+//     'a',
+//     'b',
+//     'c',
+//     'd',
+//     'e',
+//     'f',
+//     'g',
+//     'h',
+//     'i',
+//     'j',
+//     'k',
+//     'l',
+//     'm',
+//     'n',
+//     'o',
+//     'p',
+//     'q',
+//     'r',
+//     's',
+//     't',
+//     'u',
+//     'v',
+//     'w',
+//     'x',
+//     'y',
+//     'z',
+//   ],
+// ];
